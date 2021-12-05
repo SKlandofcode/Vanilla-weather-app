44b6f2fe1e56296450673bb975286233ccbf3383
SKlandofcode <87724064+SKlandofcode@users.noreply.github.com>
2021-12-05 01:02:26 +0000

    api and formatDate function added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 //alert("Hello World!");
+function formatDate() {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getUTCDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+let apiKey = "17f552ef89331cc49e9e6cef4632dee9";
