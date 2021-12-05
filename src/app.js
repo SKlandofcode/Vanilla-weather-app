cfb51a0511b2643de54164de4469aae4a27b5936
SKlandofcode <87724064+SKlandofcode@users.noreply.github.com>
2021-12-05 17:22:42 +0000

    Geo Search added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 //alert("Hello World!");
-function formatDate() {
-  let date = new Date();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -19,7 +19,51 @@ function formatDate() {
     "Friday",
     "Saturday",
   ];
-  let day = days[date.getUTCDay()];
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-let apiKey = "17f552ef89331cc49e9e6cef4632dee9";
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector(".temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity + "%";
+  windElement.innerHTML = Math.round(response.data.wind.speed) + "mph";
+  dateElement.innerHTML = formatDate(
+    response.data.dt * 1000 + response.data.timezone * 1000
+  );
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+}
+
+function search(city) {
+  let apiKey = "17f552ef89331cc49e9e6cef4632dee9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector(".search-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("New york");
