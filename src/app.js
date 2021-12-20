a042ed47341b6445a63f31fbeede66d844b953f0
SKlandofcode <87724064+SKlandofcode@users.noreply.github.com>
2021-12-20 17:34:28 +0000

    Forecast api integration

--- src/app.js
+++ src/app.js
@@ -23,6 +23,59 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+//functions- Forecast display
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+         <div class="col">
+        <div class="forecast-time">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="55"
+        />
+        <div class="forecast-temperature">
+          <span class="forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "17f552ef89331cc49e9e6cef4632dee9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+//function current temp
 function displayTemperature(response) {
   let temperatureElement = document.querySelector(".temperature");
   let cityElement = document.querySelector("#city");
